bc75e717f30f8b44d67889d138cde2f36f0ada7b
Jan-Espen Oversand <sigsegv@radiotube.org>
2025-07-14 12:19:45 +0200

    Pushd not found

--- release/buildtest.sh
+++ release/buildtest.sh
@@ -5,12 +5,12 @@ version=$2
 
 rm -rf vtun-ng-$version
 tar xzvf $assetpath/vtun-ng-$version.tar.gz || exit 1
-pushd vtun-ng-$version || exit 1
+cd vtun-ng-$version || exit 1
 ./configure || exit 1
 make || exit 1
 if [ ! -f vtunngd ]; then
   echo 'Did not build the binary'
   exit 1
 fi
-popd
+cd ..
 rm -rf vtun-ng-$version
